d633e385056688788f05980596e8c68811374ab0
calexanderberg <carlalexanderberg@gmail.com>
2022-06-01 13:37:45 +0200

    added gits

---
+++ gits/gits.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+echo "Write your commit message"
+
+read txt
+
+echo ""
+echo "Running command: git add ."
+git add .
+echo "Running command: git commit -m \"$txt\""
+git commit -m "$txt"
+echo "Running command: git push"
+#git push
