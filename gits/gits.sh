ec9e447b9abb8ab75c904b16ea5727f736362296
calexanderberg <carlalexanderberg@gmail.com>
2022-06-01 16:44:02 +0200

    Testing a new version of gits

--- gits/gits.sh
+++ gits/gits.sh
@@ -2,16 +2,43 @@
 
 #A command for adding, commiting, and pushing your files to github.
 
-echo "Write your commit message"
+echo ""
+echo "Welcome to gits"
+echo "Running: git pull"
+git pull
 
-read txt
+echo ""
+echo "Running: git status"
+git status
 
 echo ""
-echo "Running command: git add ."
-git add .
-echo ""
-echo "Running command: git commit -m \"$txt\""
-git commit -m "$txt"
-echo ""
-echo "Running command: git push"
-git push
+echo "Do you want to add all your changes to the repository? (y/n)" 
+
+read decision
+
+if [[ $decision == "yes" || $decision == "y" ]]; then
+
+	echo ""
+	echo "Write your commit message"
+	read txt
+
+	echo ""
+	echo "Running: git add ."
+	git add .
+
+	echo ""
+	echo "Running: git commit -m \"$txt\""
+	git commit -m "$txt"
+
+	echo ""
+	echo "Running: git push"
+	git push
+	
+	echo ""
+	echo "shutting down"
+	exit 1
+
+else 
+	echo "shutting down"
+	exit 1
+fi
